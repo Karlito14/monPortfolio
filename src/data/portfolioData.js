671bea7550cb556b672699b10c55f4677dd000a1
Karlito14 <leiroz26@hotmail.com>
2023-06-21 17:08:50 +0200

    Realisation de la page compétence

---
+++ src/data/portfolioData.js
@@ -0,0 +1,61 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'Portfolio Architecte d\'Intérieur',
+    languages: ['javascript'],
+    languagesIcons: ['fa-brands fa-js'],
+    source: 'https://karlito14.github.io/portfolioArchitecte/',
+    git: 'https://github.com/Karlito14/portfolioArchitecte.git',
+    info: 'Réalisation d\'un portoflio d\'une architecte d\'intérieur. Récupération les données utilisateurs via un formulaire, manipulation du DOM, gérer les événements utilisateurs.',
+    picture: '../../media/portfolioArchitecte.jpg',
+  },
+  {
+    id: 2,
+    name: 'Jeu Snake',
+    languages: ['javascript'],
+    languagesIcons: ['fa-brands fa-js'],
+    source: 'https://karlito14.github.io/jeuSnake/',
+    git: 'https://github.com/Karlito14/jeuSnake.git',
+    info: 'Le célèbre jeu Snake réalisé totalement en JavaScript avec des fonctions constructeurs.',
+    picture: '../../media/jeuSnake.jpg',
+  },
+  {
+    id: 3,
+    name: 'Liste de Courses',
+    languages: ['javascript'],
+    languagesIcons: ['fa-brands fa-js'],
+    source: 'https://karlito14.github.io/ListeCourses/',
+    git: 'https://github.com/Karlito14/ListeCourses.git',
+    info: 'Liste de courses codé en JavaScript. Possibilité de déplacer les items, de modifier le nom, la quantité et les unités.',
+    picture: '../../media/listeCourses.jpg'
+  },
+  {
+    id: 4,
+    name: 'Booki',
+    languages: ['html', 'css'],
+    languagesIcons: ["fa-brands fa-css3-alt",  "fa-brands fa-html5"],
+    source: 'https://karlito14.github.io/projetBooki/',
+    git : 'https://github.com/Karlito14/projetBooki.git',
+    info: 'Réalisation de la page d\'accueil d\'une agence de voyage totalement responsive en HTML / CSS',
+    picture: '../../media/booki.jpg'
+  },
+  {
+    id: 5,
+    name: 'Kasa',
+    languages: ['javascript','react', 'css'],
+    languagesIcons: ['fa-brands fa-js', 'fa-brands fa-react', 'fa-brands fa-css3-alt'],
+    source: 'http://www.github.com',
+    git: 'https://github.com/Karlito14/Kasa.git',
+    info: 'Réalisation d\'un site de location immobilière en React. Initialiser une application avec Create React App, configurer la navigation entre les pages de l\'application avec React Router, développer des éléments de l\'interface d\'un site web grâce à des composants React.',
+    picture: '../../media/kasa.jpg'
+  },
+  {
+    id: 6,
+    name: 'test.io',
+    languages: ['javascript','react', 'css'],
+    languagesIcons: ['fa-brands fa-js','fa-brands fa-react','fa-brands fa-css3-alt'],
+    source: 'http://www.github.com',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    picture: './media/project6.JPG'
+  },
+]
---
+++ src/pages/Projets.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Projets = () => {
+    return(
+        <Navigation />
+    )
+}
+
+export default Projets;
